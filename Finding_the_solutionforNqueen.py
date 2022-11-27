a8c4cd57117995186df8ea2c531bf3eb56dd8af7
sachal12 <93744891+sachal12@users.noreply.github.com>
2022-11-27 15:16:23 +0530

    Create Finding_the_solutionforNqueen.py

---
+++ Finding_the_solutionforNqueen.py
@@ -0,0 +1,43 @@
+# taking input from the user from user for number of queens
+
+N=int(input("Enter number of queens "))
+
+
+# defining the chessboard
+# NxN matrix with all elements 0
+board = [[0]*N for _ in range(N)]
+
+# check if the cell (i,j) is under attack by any other queen or not 
+def is_attack(i, j):
+    #checking if there is a queen in row or column
+    for k in range(0,N):
+        if board[i][k]==1 or board[k][j]==1:
+            return True
+    #checking diagonals
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if board[k][l]==1:
+                    return True
+    return False
+
+def N_queen(n):
+    # if n is 0, it means all queens are placed and the solution is found
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            
+            if (not(is_attack(i,j))) and (board[i][j]!=1):
+                board[i][j] = 1
+                # recursion
+                # wether we can put the next queen with this arrangment or not
+                if N_queen(n-1)==True:
+                    return True
+                board[i][j] = 0
+
+    return False
+
+N_queen(N)
+for i in board:
+    print (i)
